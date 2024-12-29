62c110295b7444336072794378e3d9e57ce3b3bf
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-12-29 09:15:40 +0600

    hello

--- Department-Store/pages/home.php
+++ Department-Store/pages/home.php
@@ -83,7 +83,7 @@
               <i class="bi bi-bag-heart text-info-emphasis fs-1 d-block px-3 py-2 bg-light rounded-circle"></i>
             </div>
             <div class="d-flex flex-column align-items-center">
-              <h4 class="fw-semibold text-white">Total Medicine</h4>
+              <h4 class="fw-semibold text-white">Total Product</h4>
               <h2 class="text-center text-white">55</h2>
               <a href="#" class="text-info">Show Details</a>
             </div>
