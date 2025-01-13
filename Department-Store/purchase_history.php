7892e241162055e25d9cf9531eb538d5d394147b
mdismail07hossain <mdismail07.hossain@gmail.com>
2025-01-13 09:19:54 +0600

    hello

--- Department-Store/purchase_history.php
+++ Department-Store/purchase_history.php
@@ -25,7 +25,7 @@
                         <head>
                             <meta charset="UTF-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <title>Order History</title>
+                            <title>Purchase History</title>
                             <!------------------------- design start  ---------------------->
                             <style>
                                 /* body {
