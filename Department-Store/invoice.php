0b1589f6686311c976428a32d9f96a51a0818757
mdismail07hossain <mdismail07.hossain@gmail.com>
2025-01-18 23:35:44 +0600

    add the purchase form

--- Department-Store/invoice.php
+++ Department-Store/invoice.php
@@ -1,11 +1,11 @@
 
 <?php include('check_user.php'); ?>
 <!-- database -->
-
 <!-- header part -->
 <?php include("./pages/common_pages/header.php"); ?>
-
 <!--navbar and sidebar part start-->
 <?php include("./pages/common_pages/navber.php"); ?>
 <?php include("./pages/common_pages/sidebar.php"); ?>
+<?php include("./pages/common_pages/footer.php");?>
+
   
